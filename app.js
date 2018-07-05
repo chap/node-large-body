fc035baa33568af1e2ad0e9c4c4c0ba6f9cc05fb
Chap Ambrose <chapambrose@gmail.com>
2018-07-05 09:51:21 -0500

    manually upgrade bodyparser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json({limit: '5mb'}));
+// app.use(bodyParser({limit: '5mb'}));
+
 app.post('/post', (req, res, next) => {
     res.send('success');
 });
