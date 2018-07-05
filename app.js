1a67dd5af7b2c7873449c185b61455d688bcc324
Chap Ambrose <chapambrose@gmail.com>
2018-07-05 12:01:56 -0500

    convert buffer to string

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<html><body><form action='/post' method='post'><textarea name='form-text'></textarea><button>submit</button></form>");
+    res.send("<form action='/post' method='post'><textarea name='form-text'></textarea><button>submit</button></form>");
 });
 
 app.post('/post', (req, res) => {
-    res.send('success');
+  let body = '';
+  req.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+  });
+  req.on('end', () => {
+      console.log(body);
+      res.end('success');
+  });
 });
 
 const port = process.env.PORT || 5000;
