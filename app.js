e13ac037e04f10643288fe20fc9c818ebcca5097
Chap Ambrose <chapambrose@gmail.com>
2018-07-05 10:05:05 -0500

    tweak app.post

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json({limit: '5mb'}));
 // app.use(bodyParser({limit: '5mb'}));
 
-app.post('/post', (req, res, next) => {
+app.post('/post', (req, res) => {
     res.send('success');
 });
 
