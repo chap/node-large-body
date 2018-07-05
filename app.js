111453e1f13c9dce817cdae1ed85af5669dbbd23
Chap Ambrose <chapambrose@gmail.com>
2018-07-05 12:12:21 -0500

    add post-end

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<form action='/post' method='post'><textarea name='form-text'></textarea><button>submit</button></form>");
+    res.send("<html><body><form action='/post' method='post'><textarea name='form-text'></textarea><button>submit</button></form>");
 });
 
+app.post('/post-original', (req, res) => {
+    res.send('success');
+});
+
+app.post('/post-end', (req, res) => {
+    res.end('success');
+});
+
+
 app.post('/post', (req, res) => {
-  let body = '';
+  let postBody = '';
   req.on('data', chunk => {
-      body += chunk.toString(); // convert Buffer to string
+      postBody += chunk.toString(); // convert Buffer to string
   });
   req.on('end', () => {
-      console.log(body);
+      console.log(postBody);
       res.end('success');
   });
 });
